741048754f5db5e8fd3fcd278df3d0cbf8b72e8e
David Han <davidhan.5.25@gmail.com>
2019-07-08 21:09:50 -0700

    Added gh-pages for deployment and finishing touches

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,12 @@
-import './ImageList.css'
-import React from 'react';
-import ImageCard from './ImageCard';
+import "./ImageList.css";
+import React from "react";
+import ImageCard from "./ImageCard";
 
-const ImageList = (props) => {
-// Destructuring each image item
-const images = props.images.map((image) => {
-    return <ImageCard key={image.id} image={image} />
+const ImageList = props => {
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
-}
+};
 
 export default ImageList;
